74c0971f315c6192ff444f77b035e15ad4cef761
Ankur Mittal <ankurmittal092@gmail.com>
2019-04-26 18:26:23 +0530

    Gulifile for publish

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var awspublish = require('gulp-awspublish');
+var merge = require('merge-stream');
+var rename = require('gulp-rename');
+var argv = require('yargs').argv;
+
+gulp.task('publish', function() {
+  if (!argv.bucket) {
+    throw Error('Please pass a --bucket arugment of the target S3 Bucket');
+  }
+
+  var publisher = awspublish.create({
+    region: process.env.S3_REGION,
+    params: {
+      Bucket: argv.bucket
+    }
+  });
+
+  var prefixDir = function(path) {
+    path.dirname = argv.dirprefix + path.dirname;
+  };
+
+  var gzipFilter = [
+    'build/**/*.js',
+    'build/**/*.html',
+    'build/**/*.css',
+    'build/**/*.ico',
+    'build/**/*.json'
+  ];
+  var negativeFilter = gzipFilter.map(function(x) {
+    return '!' + x;
+  });
+
+  var plain = gulp
+    .src(['build/**/*'].concat(negativeFilter))
+    .pipe(rename(prefixDir));
+  var gzip = gulp
+    .src(gzipFilter)
+    .pipe(rename(prefixDir))
+    .pipe(awspublish.gzip());
+
+  return merge(gzip, plain)
+    .pipe(publisher.publish())
+    .pipe(publisher.sync())
+    .pipe(awspublish.reporter());
+});
